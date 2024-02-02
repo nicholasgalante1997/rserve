ae58a6849e43707658969267d233a544cfa53577
Nick G <nicholasgalante1997@gmail.com>
2024-02-01 20:31:29 -0700

    feat: Initial empty cargo app

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use server::Server;
+
+pub mod server;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/server.rs
@@ -0,0 +1,7 @@
+use std::net::{TcpListener, TcpStream};
+
+pub struct Server {
+    _instance: TcpListener,
+}
+
+impl Server {}
